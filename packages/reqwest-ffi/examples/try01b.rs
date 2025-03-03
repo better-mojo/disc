43301244979dd2577f1631b056cbcec40a78d84c
hhglory <hhglory@outlook.com>
2025-03-03 15:52:23 +0800

    update

--- packages/reqwest-ffi/examples/try01b.rs
+++ packages/reqwest-ffi/examples/try01b.rs
@@ -7,18 +7,34 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .json::<HashMap<String, String>>()
         .await?;
 
-    println!("http response:\n{resp:#?}");
+    println!("http get response:\n{resp:#?}");
 
 
+    // ref: https://docs.rs/reqwest/latest/reqwest/#forms
     // This will POST a body of `foo=bar&baz=quux`
     let params = [("foo", "bar"), ("baz", "quux")];
     let client = reqwest::Client::new();
-    let res = client.post("http://httpbin.org/post")
+    let res1 = client.post("http://httpbin.org/post")
         .form(&params)
         .send()
         .await?;
 
-    println!("http response:\n{res:#?}");
+    println!("http post + form response:\n{res1:#?}");
+
+
+    // ref: https://docs.rs/reqwest/latest/reqwest/#json
+    // This will POST a body of `{"lang":"rust","body":"json"}`
+    let mut map = HashMap::new();
+    map.insert("lang", "rust");
+    map.insert("body", "json");
+
+    let client = reqwest::Client::new();
+    let res2 = client.post("http://httpbin.org/post")
+        .json(&map)
+        .send()
+        .await?;
+
+    println!("http post + json response:\n{res2:#?}");
 
 
     Ok(())
